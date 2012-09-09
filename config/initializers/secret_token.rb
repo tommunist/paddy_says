5e678163a351cf6d027e7beb3816b1c5b9e6467b
Tom Ryan <tommy@hypersynergy.net>
2012-09-10 08:45:33 +1000

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PaddySays::Application.config.secret_token = '4b7664c3cb548e6a7e02a0c76c9aa349966c183108939524026a2e069e2a49d60759058e921f570aa63adc9751524b33d01709c3dcb0b4e58c0764c245119d37'
